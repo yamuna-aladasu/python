0927f16069720329252eca8d0842d7d79a090b58
yamuna aladasu <yamuna.aladasu@bigthinkcode.com>
2026-04-06 17:35:14 +0530

    strings

---
+++ stringoperations.py
@@ -0,0 +1,17 @@
+x=" stringg"
+print(x[:4])
+print(x[::-1])
+print(x[:-1])
+print(x[0:-1:2])
+print(x.upper())
+print(x.lower())
+print(x.strip())
+print(x.replace("g","k"))
+#f string
+c=20
+print(f"my age is {c} old")
+#boolean 
+print(10>9)
+print(20>110)
+print(20==10)
+print(0==0)
